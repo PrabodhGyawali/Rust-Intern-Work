c503ac14d05257ef1cc8f1be22f1752b5721dad7
root <root@AlienMaster40>
2024-06-20 16:35:48 +0100

    audio

---
+++ numerical-quiz/src/main.rs
@@ -0,0 +1,85 @@
+use std::env;
+use std::fs::File;
+use serde::Deserialize;
+use serde_derive::Deserialize;
+use std::io::BufReader;
+use std::io;
+
+// use std::error::Error;
+// use std::{io, process::Output};
+// use rodio::{Decoder, OutputStream, source::Source};
+
+// Important to understand the Source Trait
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+struct QuizQuestion {
+    song_path: String,
+    options: Vec<String>, 
+    correct_answer: String,
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    // Check the number of arguments
+    if !(args.len() == 2 || args.len() == 3) {
+        eprintln!("Usage: cargo run <sound_file> [option]");
+        return;
+    }
+    // Check for the help message
+    if args[1] == "-h" {
+        println!("Help message: This program plays sounds from a file.\n\
+                  cargo run [path] [options] -> the path to the file relative to cargo.toml\n\
+                  --no-message -> optional parameter to not display message");
+        return;
+    } 
+    // println!("{:?}", args);
+    if args.len() == 3 && args[2] != "--no-message" {
+        eprintln!("Invalid option");
+        return;
+    } else {  println!("This is a numerical quiz game on songs.") }
+    
+    
+    // Get the sound file path from the command line argument
+    let sound_file_result: Result<File, std::io::Error> = File::open(&args[1]);
+    
+    let sound_file = match sound_file_result {
+        Ok(file) => file,
+        Err(error) => panic!("File not found: {error:?}")
+    };
+    // Get data from the sound_file
+    let reader = BufReader::new(sound_file);
+    let quiz_data: Vec<QuizQuestion> = match serde_json::from_reader(reader) {
+        Ok(json) => json,
+        Err(_error) => panic!("File not Serializable: {_error:?}"),
+    };
+    
+    let mut score: i8  = 0;
+    // TODO: Add a 10 second timer
+    for (index, question) in quiz_data.iter().enumerate() {
+        println!("Question {}: Guess the song. Press 'r' to repeat.", index + 1);
+        
+        for (index, option) in question.options.iter().enumerate() {
+            println!("{}: {}", index + 1, option);
+        }
+        // Obtain User Input
+        let mut user_input = String::new();
+        io::stdin().read_line(&mut user_input)
+            .expect("Failed to read user input");
+
+        let trimmed_input = user_input.trim();
+        if trimmed_input == question.correct_answer { 
+            println!("Correct!");  
+            score += 1;
+        } 
+        else {
+            println!("Incorrect!");
+        }
+    }
+}
+
+    // // Get an output stream handle to the default physical sound device
+    // let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    // // Load a sound from a file, using a path relative to Cargo.toml
+    // println!("{:?}", args);
+    
+
---
+++ numerical-quiz/tests/main_tests.rs
@@ -0,0 +1,44 @@
+use std::process::Command;
+use std::process::Output;
+use std::str;
+
+
+#[test]
+fn test_args_path_no_message() {
+    let output = Command::new("cargo")
+        .args(&["run", "--", "arg1", "--no-message"])
+        .output()
+        .expect("");
+    let ouput_string = str::from_utf8(&output.stdout).unwrap();
+    println!("{:?}", ouput_string);
+    assert!(!ouput_string.contains("This is a numerical quiz game on songs.\n"));
+}
+
+#[test]
+fn test_invalid_arg() {
+    let output = Command::new("cargo")
+        .args(&["run", "--", "arg1", "--invalid"])
+        .output()
+        .expect("");
+    let err_string = str::from_utf8(&output.stderr).unwrap();
+    println!("{:?}", err_string);
+    assert!(err_string.contains("Invalid option\n"));
+}
+
+#[test]
+fn test_args_h() {
+    let output = Command::new("cargo")
+        .args(&["run", "--", "-h"])
+        .output()
+        .expect("Help message: This program plays sounds from a file.\n\
+                  cargo run [path] [options] -> the path to the file relative to cargo.toml\n\
+                  --no-message -> optional parameter to not display message");
+    let output_str = str::from_utf8(&output.stdout).unwrap();
+    let expected_output = "Help message: This program plays sounds from a file.\n\
+                  cargo run [path] [options] -> the path to the file relative to cargo.toml\n\
+                  --no-message -> optional parameter to not display message";
+    println!("{:?}", expected_output);
+    assert_eq!(output_str.trim(), expected_output);
+}
+
+
